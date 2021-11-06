7c9aed74577c57b897dca2de6274fc63516e9392
Arunsh93 <Arunhosur93@gmail.com>
2021-11-06 10:57:08 +0530

    Product review management program using Linq Learning program

---
+++ ProductReviewManagement/Management.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductManagement> listProductReview)
+        {
+            var recordsData = (from productreviews in listProductReview
+                               orderby productreviews.Rating descending
+                               select productreviews).Take(3);
+
+            foreach (var list in recordsData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            }
+        }
+
+        public void SelectedRecords(List<ProductManagement> listProductReview)
+        {
+            var recordData = from productReviews in listProductReview
+                             where (productReviews.ProductID == 1 || productReviews.ProductID == 4 || productReviews.ProductID == 9) &&
+                             productReviews.Rating > 3
+                             select productReviews;
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            }
+        }
+
+        public void RetrieveCountOfRecords(List<ProductManagement> listProductReview)
+        {
+            var RecordData = listProductReview.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+
+
+            foreach (var list in RecordData)
+            {
+                Console.WriteLine(list.ProductID + "------- " + list.Count);
+            }
+        }
+
+        public void RetrieveProductID_Review(List<ProductManagement> listProductReview)
+        {
+            var recordedData = listProductReview.Select(x => new { ProductID = x.ProductID, ProductReview = x.Review });
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "Review:- " + list.ProductReview);
+            }
+        }
+
+        public void SkipTop5Records(List<ProductManagement> listProductReview)
+        {
+
+            var recordedData = (from productReviews in listProductReview select productReviews).Skip(5).ToList(); ;
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
+                     + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+        }
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product review management problem statement");
+
+            List<ProductManagement> ProductReviewlist = new List<ProductManagement>()
+            {
+                 new ProductManagement(){ProductID=1,UserID=1,Rating=5,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=2,UserID=1,Rating=4,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=3,UserID=2,Rating=2,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=4,UserID=2,Rating=4,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=5,UserID=3,Rating=2,Review="nice",isLike=false},
+                 new ProductManagement(){ProductID=9,UserID=4,Rating=5,Review="Bad",isLike=false},
+                 new ProductManagement(){ProductID=1,UserID=3,Rating=1,Review="Good",isLike=false},
+                 new ProductManagement(){ProductID=11,UserID=10,Rating=3,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=10,UserID=10,Rating=3,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=12,UserID=10,Rating=3,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=13,UserID=10,Rating=3,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=14,UserID=10,Rating=3,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=15,UserID=10,Rating=3,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=13,UserID=1,Rating=2,Review="Good",isLike=true},
+                 new ProductManagement(){ProductID=16,UserID=10,Rating=3,Review="Good",isLike=true}
+            };
+            //foreach(var list in ProductReviewlist)
+            //{
+            //    Console.WriteLine("ProductID:-"+list.ProductID+" "+"UserID:-"+list.UserID+" "+"Rating:-"+list.Rating+" "+"Review:-"+list.Review+" "+"isLike:-"+list.isLike);
+            //}
+
+            Management management = new Management();
+            //Uc-2
+            //management.TopRecords(ProductReviewlist);
+
+            //uc-3
+            //management.SelectedRecords(ProductReviewlist);
+
+            //uc-4
+            management.RetrieveCountOfRecords(ProductReviewlist);
+
+            //UC5
+            management.RetrieveProductID_Review(ProductReviewlist);
+
+            //UC6
+            management.SkipTop5Records(ProductReviewlist);
+        }
+    }
+}
